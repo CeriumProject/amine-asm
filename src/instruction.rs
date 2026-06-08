dfd302a6c5c774003940fa4d97dbc076d4ccde52
Louis Goerlt <louis.goerlt@gmail.com>
2026-06-08 08:28:42 +0200

    clone project files from https://github.com/cc-aegis/amine

---
+++ src/instruction.rs
@@ -0,0 +1,128 @@
+use std::str::FromStr;
+use crate::AssemblerError;
+use crate::opcode::{NoOpOpcode, SingleOpOpcode, TwoOpOpcode};
+use crate::operand::{ConstOp, RegOp};
+
+#[derive(Debug)]
+pub enum Instruction {
+    Label(String),
+    Sublabel(String),
+    Include(String),
+    Define(String, ConstOp),
+    RawWords(Vec<ConstOp>),
+    TwoOp(TwoOpOpcode, RegOp, RegOp),
+    SingleOp(SingleOpOpcode, RegOp),
+    NoOp(NoOpOpcode),
+    Blank,
+}
+
+#[derive(Debug)]
+pub struct InvalidInstruction(String);
+
+impl FromStr for Instruction {
+    type Err = AssemblerError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.split(';').next().unwrap().trim();
+        //todo: match this (pattern if cond => val)
+        if let Some(label) = s.strip_suffix(':') {
+            return if let Some(label) = label.strip_prefix('.') {
+                Ok(Instruction::Sublabel(label.to_string()))
+            } else {
+                Ok(Instruction::Label(label.to_string()))
+            }
+        }
+        let parts = s.split_whitespace().collect::<Vec<_>>();
+        match parts.as_slice() {
+            ["include", file] => Ok(Instruction::Include(file.to_string())),
+            ["define", lhs, rhs] => Ok(Instruction::Define(lhs.to_string(), rhs.parse()?)),
+            ["dw", ..] => Ok(Instruction::RawWords(parts
+                .into_iter()
+                .skip(1)
+                .map(str::parse)
+                .collect::<Result<_, _>>()?
+            )),
+            [opcode, lhs, rhs] => Ok(Instruction::TwoOp(match *opcode {
+                "mov" => TwoOpOpcode::Mov,
+                "pusht" => TwoOpOpcode::PushT,
+                "popt" => TwoOpOpcode::PopT,
+                "read" => TwoOpOpcode::Read,
+                "write" => TwoOpOpcode::Write,
+                "copy" => TwoOpOpcode::Copy,
+                "swap" => TwoOpOpcode::Swap,
+                "readitr" => TwoOpOpcode::ReadItr,
+                "writeitr" => TwoOpOpcode::WriteItr,
+                "copyitr" => TwoOpOpcode::CopyItr,
+                "readg" => TwoOpOpcode::ReadG,
+                "writeg" => TwoOpOpcode::WriteG,
+                "lookup" => TwoOpOpcode::Lookup,
+                "jlookup" => TwoOpOpcode::JLookup,
+                "clookup" => TwoOpOpcode::CLookup,
+                "jrnzdec" => TwoOpOpcode::JrnzDec,
+                "callw" => TwoOpOpcode::CallW,
+                "jrz" => TwoOpOpcode::Jrz,
+                "jrnz" => TwoOpOpcode::Jrnz,
+                "jrgt" => TwoOpOpcode::Jrgt,
+                "jrge" => TwoOpOpcode::Jrge,
+                "jrlt" => TwoOpOpcode::Jrlt,
+                "jrle" => TwoOpOpcode::Jrle,
+                "add" => TwoOpOpcode::Add,
+                "sub" => TwoOpOpcode::Sub,
+                "mul" => TwoOpOpcode::Mul,
+                "div" => TwoOpOpcode::Div,
+                "utof" => TwoOpOpcode::Utof,
+                "itof" => TwoOpOpcode::Itof,
+                "imul" => TwoOpOpcode::Imul,
+                "idiv" => TwoOpOpcode::Idiv,
+                "fadd" => TwoOpOpcode::Fadd,
+                "fsub" => TwoOpOpcode::Fsub,
+                "fmul" => TwoOpOpcode::Fmul,
+                "fdiv" => TwoOpOpcode::Fdiv,
+                "ftou" => TwoOpOpcode::Ftou,
+                "ftoi" => TwoOpOpcode::Ftoi,
+                "and" => TwoOpOpcode::And,
+                "or" => TwoOpOpcode::Or,
+                "xor" => TwoOpOpcode::Xor,
+                "inv" => TwoOpOpcode::Inv,
+                "bool" => TwoOpOpcode::Bool,
+                "neg" => TwoOpOpcode::Neg,
+                "shl" => TwoOpOpcode::Shl,
+                "shr" => TwoOpOpcode::Shr,
+                "cmp" => TwoOpOpcode::Cmp,
+                "ctx" => TwoOpOpcode::Ctx,
+                _ => Err(AssemblerError::InvalidInstruction(InvalidInstruction(s.to_string())))?,
+            }, lhs.parse().map_err(AssemblerError::InvalidOperand)?, rhs.parse().map_err(AssemblerError::InvalidOperand)?)),
+            [opcode, op] => Ok(Instruction::SingleOp(match *opcode {
+                "dbg" => SingleOpOpcode::Dbg,
+                "push" => SingleOpOpcode::Push,
+                "pop" => SingleOpOpcode::Pop,
+                "cmpz" => SingleOpOpcode::CmpZ,
+                "inc" => SingleOpOpcode::Inc,
+                "dec" => SingleOpOpcode::Dec,
+                "sshl" => SingleOpOpcode::SShl,
+                "sshr" => SingleOpOpcode::SShr,
+                "floor" => SingleOpOpcode::Floor,
+                "ceil" => SingleOpOpcode::Ceil,
+                "jz" => SingleOpOpcode::Jz,
+                "jnz" => SingleOpOpcode::Jnz,
+                "jgt" => SingleOpOpcode::Jgt,
+                "jge" => SingleOpOpcode::Jge,
+                "jlt" => SingleOpOpcode::Jlt,
+                "jle" => SingleOpOpcode::Jle,
+                "jmp" => SingleOpOpcode::Jmp,
+                "call" => SingleOpOpcode::Call,
+                "retv" => SingleOpOpcode::RetV,
+                _ => Err(AssemblerError::InvalidInstruction(InvalidInstruction(s.to_string())))?,
+            }, op.parse().map_err(AssemblerError::InvalidOperand)?)),
+            [opcode] => Ok(Instruction::NoOp(match *opcode {
+                "nop" => NoOpOpcode::Nop,
+                "ret" => NoOpOpcode::Ret,
+                "send" => NoOpOpcode::Send,
+                "exit" => NoOpOpcode::Exit,
+                _ => Err(AssemblerError::InvalidInstruction(InvalidInstruction(s.to_string())))?,
+            })),
+            [] => Ok(Instruction::Blank),
+            _ => Err(AssemblerError::InvalidInstruction(InvalidInstruction(s.to_string())))?,
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,93 @@
+#![feature(f16)]
+
+use std::collections::HashSet;
+use std::path::Path;
+use crate::instruction::Instruction;
+
+mod instruction;
+mod opcode;
+mod operand;
+mod preparse;
+mod assemble;
+
+// TODO: move all error variants into here
+#[derive(Debug)]
+pub enum AssemblerError {
+    FileNotFound(String),
+    InvalidInstruction(instruction::InvalidInstruction),
+    InvalidOperand(operand::InvalidOperand),
+    UnallowedRegister(operand::UnallowedRegister),
+    InvalidLabel(operand::InvalidLabel),
+    UnallowedInclude(preparse::UnallowedInclude),
+    CodeTooLong,
+}
+
+fn parse_file(mut root_dir: &Path, file: &str) -> Result<Vec<Instruction>, AssemblerError> {
+    std::fs::read_to_string(&root_dir.join(file))
+        .map_err(|_| file.to_string())
+        .map_err(AssemblerError::FileNotFound)?
+        .lines()
+        .map(str::parse)
+        .collect()
+}
+
+fn link_rec(root_dir: &Path, file: String, blacklist: &mut HashSet<String>) -> Result<Vec<Instruction>, AssemblerError> {
+    if blacklist.contains(&file) {
+        return Ok(Vec::new());
+    }
+
+    let parsed = parse_file(root_dir, &file)?;
+
+    blacklist.insert(file);
+
+    let result = parsed
+        .into_iter()
+        .map(|inst| match inst {
+            Instruction::Include(file) => link_rec(root_dir, file, blacklist),
+            _ => Ok(vec![inst]),
+        })
+        .collect::<Result<Vec<Vec<Instruction>>, AssemblerError>>()?
+        .into_iter()
+        .flatten()
+        .collect::<Vec<Instruction>>();
+
+    Ok(result)
+}
+
+fn link(root_dir: &Path) -> Result<Vec<Instruction>, AssemblerError> {
+    // parse_file(root_dir, "main.s")
+    link_rec(root_dir, String::from("main.s"), &mut HashSet::new())
+}
+
+fn assemble(code: &[Instruction]) -> Result<Box<[u16; 65536]>, AssemblerError> {
+    let code = assemble::assemble(code)?;
+    let mut result = Box::new([0; 65536]);
+
+    match code.len() {
+        65537.. => Err(AssemblerError::CodeTooLong),
+        len => {
+            result[..len].copy_from_slice(&code);
+            Ok(result)
+        }
+    }
+}
+
+pub fn assemble_file(dir: &Path) -> Result<Box<[u16; 65536]>, AssemblerError> {
+    let parent = dir
+        .parent()
+        .ok_or_else(|| AssemblerError::FileNotFound(dir.display().to_string()))?;
+    let file = dir
+        .file_name()
+        .ok_or_else(|| AssemblerError::FileNotFound(dir.display().to_string()))?
+        .to_str()
+        .ok_or_else(|| AssemblerError::FileNotFound(dir.display().to_string()))?;
+    parse_file(parent, file)
+        .map(|files| assemble(&files))
+        .flatten()
+}
+
+pub fn assemble_project(root_dir: &Path) -> Result<Box<[u16; 65536]>, AssemblerError> {
+    link(root_dir)
+        .map(|files| assemble(&files))
+        .flatten()
+}
